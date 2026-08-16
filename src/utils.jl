88dcfb8a6ef4c0fa4efb8c6b8dca860ed8b3e2b8
phlaster <125278254+phlaster@users.noreply.github.com>
2026-08-16 21:46:17 +0300

    revert formatter mess in utils

--- src/utils.jl
+++ src/utils.jl
@@ -46,21 +46,10 @@ const BASES_W_GAPS = (ALL_BASES..., '-')
 Map each IUPAC nucleotide character to a tuple of its constituent non-degenerate bases (e.g., `'R' => ("A", "G")`).
 """
 const IUPAC_B2V = Dict(
-    'A' => Tuple("A"),
-    'C' => Tuple("C"),
-    'G' => Tuple("G"),
-    'T' => Tuple("T"),
-    'R' => Tuple("AG"),
-    'Y' => Tuple("CT"),
-    'S' => Tuple("CG"),
-    'W' => Tuple("AT"),
-    'K' => Tuple("GT"),
-    'M' => Tuple("AC"),
-    'B' => Tuple("CGT"),
-    'D' => Tuple("AGT"),
-    'H' => Tuple("ACT"),
-    'V' => Tuple("ACG"),
-    'N' => Tuple("ACGT"),
+    'A'=>Tuple("A"),  'C'=>Tuple("C"),  'G'=>Tuple("G"),  'T'=>Tuple("T"),
+    'R'=>Tuple("AG"), 'Y'=>Tuple("CT"), 'S'=>Tuple("CG"), 'W'=>Tuple("AT"),
+    'K'=>Tuple("GT"), 'M'=>Tuple("AC"), 'B'=>Tuple("CGT"),'D'=>Tuple("AGT"),
+    'H'=>Tuple("ACT"),'V'=>Tuple("ACG"),'N'=>Tuple("ACGT")
 )
 
 """
@@ -89,126 +78,24 @@ const IUPAC_GC_CONTENT = Dict(k => count(in("CG"), v) / length(v) for (k, v) in 
 
 Store a 256-element lookup table (`Vector{UInt8}`) for fast, branchless complementation of ASCII DNA characters, including degenerate IUPAC codes and gaps (`-`).
 """
-const DNA_COMP_TABLE_DEG = let
-    _comp_dna_deg(b::UInt8)::UInt8 = b == UInt8('A') ?
-        UInt8('T') :
-        b == UInt8('a') ?
-            UInt8('T') :
-            b == UInt8('T') ?
-                UInt8('A') :
-                b == UInt8('t') ?
-                    UInt8('A') :
-                    b == UInt8('C') ?
-                        UInt8('G') :
-                        b == UInt8('c') ?
-                            UInt8('G') :
-                            b == UInt8('G') ?
-                                UInt8('C') :
-                                b == UInt8('g') ?
-                                    UInt8('C') :
-                                    b == UInt8('R') ?
-                                        UInt8('Y') :
-                                        b == UInt8('r') ?
-                                            UInt8('Y') :
-                                            b == UInt8('Y') ?
-                                                UInt8('R') :
-                                                b == UInt8('y') ?
-                                                    UInt8('R') :
-                                                    b == UInt8('S') ?
-                                                        UInt8('S') :
-                                                        b == UInt8('s') ?
-                                                            UInt8('S') :
-                                                            b == UInt8('W') ?
-                                                                UInt8('W') :
-                                                                b == UInt8('w') ?
-                                                                    UInt8('W') :
-                                                                    b == UInt8('K') ?
-                                                                        UInt8('M') :
-                                                                        b == UInt8('k') ?
-                                                                            UInt8('M') :
-                                                                            b ==
-                                                                                UInt8('M') ?
-                                                                                UInt8('K') :
-                                                                                b ==
-                                                                                    UInt8(
-                                                                                        'm',
-                                                                                    ) ?
-                                                                                    UInt8(
-                                                                                        'K',
-                                                                                    ) :
-                                                                                    b ==
-                                                                                        UInt8(
-                                                                                            'B',
-                                                                                        ) ?
-                                                                                        UInt8(
-                                                                                            'V',
-                                                                                        ) :
-                                                                                        b ==
-                                                                                            UInt8(
-                                                                                                'b',
-                                                                                            ) ?
-                                                                                            UInt8(
-                                                                                                'V',
-                                                                                            ) :
-                                                                                            b ==
-                                                                                                UInt8(
-                                                                                                    'D',
-                                                                                                ) ?
-                                                                                                UInt8(
-                                                                                                    'H',
-                                                                                                ) :
-                                                                                                b ==
-                                                                                                    UInt8(
-                                                                                                        'd',
-                                                                                                    ) ?
-                                                                                                    UInt8(
-                                                                                                        'H',
-                                                                                                    ) :
-                                                                                                    b ==
-                                                                                                        UInt8(
-                                                                                                            'H',
-                                                                                                        ) ?
-                                                                                                        UInt8(
-                                                                                                            'D',
-                                                                                                        ) :
-                                                                                                        b ==
-                                                                                                            UInt8(
-                                                                                                                'h',
-                                                                                                            ) ?
-                                                                                                            UInt8(
-                                                                                                                'D',
-                                                                                                            ) :
-                                                                                                            b ==
-                                                                                                                UInt8(
-                                                                                                                    'V',
-                                                                                                                ) ?
-                                                                                                                UInt8(
-                                                                                                                    'B',
-                                                                                                                ) :
-                                                                                                                b ==
-                                                                                                                    UInt8(
-                                                                                                                        'v',
-                                                                                                                    ) ?
-                                                                                                                    UInt8(
-                                                                                                                        'B',
-                                                                                                                    ) :
-                                                                                                                    b ==
-                                                                                                                        UInt8(
-                                                                                                                            'N',
-                                                                                                                        ) ?
-                                                                                                                        UInt8(
-                                                                                                                            'N',
-                                                                                                                        ) :
-                                                                                                                        b ==
-                                                                                                                            UInt8(
-                                                                                                                                'n',
-                                                                                                                            ) ?
-                                                                                                                            UInt8(
-                                                                                                                                'N',
-                                                                                                                            ) :
-                                                                                                                            UInt8(
-                                                                                                                                '-',
-                                                                                                                            )
+const DNA_COMP_TABLE_DEG = let 
+    _comp_dna_deg(b::UInt8)::UInt8 =
+    b == UInt8('A') ? UInt8('T') : b == UInt8('a') ? UInt8('T') :
+    b == UInt8('T') ? UInt8('A') : b == UInt8('t') ? UInt8('A') :
+    b == UInt8('C') ? UInt8('G') : b == UInt8('c') ? UInt8('G') :
+    b == UInt8('G') ? UInt8('C') : b == UInt8('g') ? UInt8('C') :
+    b == UInt8('R') ? UInt8('Y') : b == UInt8('r') ? UInt8('Y') :
+    b == UInt8('Y') ? UInt8('R') : b == UInt8('y') ? UInt8('R') :
+    b == UInt8('S') ? UInt8('S') : b == UInt8('s') ? UInt8('S') :
+    b == UInt8('W') ? UInt8('W') : b == UInt8('w') ? UInt8('W') :
+    b == UInt8('K') ? UInt8('M') : b == UInt8('k') ? UInt8('M') :
+    b == UInt8('M') ? UInt8('K') : b == UInt8('m') ? UInt8('K') :
+    b == UInt8('B') ? UInt8('V') : b == UInt8('b') ? UInt8('V') :
+    b == UInt8('D') ? UInt8('H') : b == UInt8('d') ? UInt8('H') :
+    b == UInt8('H') ? UInt8('D') : b == UInt8('h') ? UInt8('D') :
+    b == UInt8('V') ? UInt8('B') : b == UInt8('v') ? UInt8('B') :
+    b == UInt8('N') ? UInt8('N') : b == UInt8('n') ? UInt8('N') :
+    UInt8('-')
     [_comp_dna_deg(UInt8(i)) for i in 0:255]
 end
 
